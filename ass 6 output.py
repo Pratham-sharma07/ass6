15de78979e2ff1d50aa18ce0eaf43001b9a76a7e
Pratham-sharma07 <102985188+Pratham-sharma07@users.noreply.github.com>
2022-07-08 23:11:54 +0530

    Add files via upload

---
+++ ass 6 output.py
@@ -0,0 +1,39 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:/Users/acer/Desktop/ass 6 input.py ================
+Enter a number: 2
+2 is a perfect number.
+Enter a string: wow superb
+wow superb is a Palindrome.
+Rows: 5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+Enter a string: ok fine
+Yes, the given string is a pangram.
+Enter hyphen-separated sequence of words : ok-fine
+fine-ok
+
+Student ID: 21107031
+Student Name: Pratham Sharma
+
+Student ID: 21107031
+Student Name: Pratham Sharma
+
+Student Name: Pratham Sharma
+Student Class: 2nd Semester
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+[[-10, 2, 8]]
+[[-10, 2, 8], [-7, -3, 10]]
+enter : {[})
+{[}) - is Unbalanced
